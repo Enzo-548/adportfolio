9965f15d443e0c562322fe1adb47c261b3125017
Enzo SP <enzosalatinopicoli@gmail.com>
2026-09-03 18:50:51 +0000

    adjustments to rotating hero

--- soda-campaign/soda-rotating-skyscraper/script.js
+++ soda-campaign/soda-rotating-skyscraper/script.js
@@ -2,4 +2,4 @@ document.querySelector('.creative').addEventListener('keydown', (event) => {
     if (event.key === 'Enter' || event.key === ' ') {
         event.currentTarget.classList.add('is-activated');
     }
-});
+})
